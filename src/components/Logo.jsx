6ff9361d64937b5befbd803ef204a47862157b79
Ankita Mishra <ankitamishra8763@gmail.com>
2026-08-22 12:02:51 +0530

    logo size updated

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,8 +5,8 @@ export default function Logo({ light = false }) {
   return (
     <Link to="/" className="flex items-center gap-3 shrink-0 group">
     
- <div>
-  <img src ="/logo1.png" alt="logo" className="h-20 w-42 sm:h-15 sm:w-45 lg:h-24 lg:w-30 object-center object-cover" />
+ <div >
+  <img src ="/logo1.png" alt="logo" className="h-18 w-45 xs:h-20 xs:w-51 sm:h-23 sm:w-48 lg:h-30 lg:w-20 md:h-20 md:w-20  object-center object-cover" />
  
  </div>
       
