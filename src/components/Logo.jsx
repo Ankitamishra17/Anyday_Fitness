dc057cc91d1050a81071d7074985104b8095042b
Ankita Mishra <ankitamishra8763@gmail.com>
2026-08-22 14:59:07 +0530

    update

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,12 +4,13 @@ import { Link } from "react-router-dom";
 export default function Logo({ light = false }) {
   return (
     <Link to="/" className="flex items-center gap-3 shrink-0 group">
-    
- <div >
-  <img src ="/logo1.png" alt="logo" className="h-18 w-[100px] xs:h-20 xs:w-51 sm:h-23 sm:w-48 lg:h-30 lg:w-[130px] md:h-20 md:w-[120px]  object-center object-cover" />
- 
- </div>
-      
+      <div>
+        <img
+          src="/logo2.png"
+          alt="logo"
+          className="h-18 w-[125px] xs:h-20 xs:w-51 sm:h-23 sm:w-48 lg:h-34 lg:w-[145px] md:h-20 md:w-[120px]  object-center object-cover"
+        />
+      </div>
     </Link>
   );
 }
