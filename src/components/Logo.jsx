0993285ec9546867122a6f7fda0b8f7902ada6d8
Ankita Mishra <ankitamishra8763@gmail.com>
2026-08-22 12:17:38 +0530

    logo size fixed

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -6,7 +6,7 @@ export default function Logo({ light = false }) {
     <Link to="/" className="flex items-center gap-3 shrink-0 group">
     
  <div >
-  <img src ="/logo1.png" alt="logo" className="h-18 w-45 xs:h-20 xs:w-51 sm:h-23 sm:w-48 lg:h-30 lg:w-20 md:h-20 md:w-20  object-center object-cover" />
+  <img src ="/logo1.png" alt="logo" className="h-18 w-[100px] xs:h-20 xs:w-51 sm:h-23 sm:w-48 lg:h-30 lg:w-[130px] md:h-20 md:w-[120px]  object-center object-cover" />
  
  </div>
       
